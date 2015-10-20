fbca2509c71bc6273a80e855b47fedba6eecb3c9
Michael Moss <realmike33@gmail.com>
2015-10-20 14:29:25 -0700

    Initial Commit

---
+++ WordPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordPlay
+//
+//  Created by Michael Moss on 10/20/15.
+//  Copyright © 2015 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WordPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WordPlay
+//
+//  Created by Michael Moss on 10/20/15.
+//  Copyright © 2015 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
